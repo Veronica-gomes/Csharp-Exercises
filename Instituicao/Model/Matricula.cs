b9cb0b94686a8f0bd67dfa3dd02dcc0135632da9
Veronica Gomes <88216333+Veronica-gomes@users.noreply.github.com>
2022-12-06 12:38:00 -0300

    1

---
+++ Instituicao/Model/Matricula.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instituicao.Model
+{
+    internal class Matricula
+    {
+        public List<Aluno> Matriculados { get; set; }
+        public List<Aluno> AlunosFormados { get; set; }
+
+        public void AdicionarAlunos(Aluno aluno)
+        {
+            Matriculados.Add(aluno);
+        }
+
+        public void AdicionarFormados(Aluno formado)
+        {
+            AlunosFormados.Add(formado);
+        }
+
+        public void ListarAlunos()
+        {
+            int matriculados = 0;
+            foreach (Aluno aluno in Matriculados)
+            {
+                Console.WriteLine($"\n {matriculados} Nome: {aluno.Nome} {aluno.Sobrenome}");
+                matriculados++;
+            }
+            Console.WriteLine($"O Total de matrículas são: {matriculados}");
+        }
+
+        public void ListarFormados()
+        {
+            int alunosFormados = 0;
+            foreach (Aluno formado in AlunosFormados)
+            {
+                Console.WriteLine($"\n {alunosFormados} Nome: {formado.Nome} {formado.Sobrenome}");
+                Console.WriteLine($"CPF: {formado.Cpf}");
+                alunosFormados++;
+            }
+            Console.WriteLine($"O total de formados são: {alunosFormados}");
+        }
+
+        public void PercentualEvasao()
+        {
+            var matriculados = Matriculados.Count;
+            var formados = AlunosFormados.Count;
+            var total = formados + matriculados;
+
+            var percEvasao = ((Double)formados / total);
+
+            Console.WriteLine($"\n O total de evasão é de: {percEvasao * 100}%.");
+
+
+            if (percEvasao < 0.10)
+            {
+                Console.WriteLine("Baixa evasão.");
+            }
+
+            if (percEvasao >= 0.10 && (percEvasao <= 0.15))
+            {
+                Console.WriteLine("Evasão média.");
+            }
+
+            if (percEvasao > 0.15 && (percEvasao <= 0.25))
+            {
+                Console.WriteLine("Evasão alta.");
+            }
+
+            if (percEvasao > 0.25)
+                Console.WriteLine("Evasão muito alta.");
+
+            else
+                Console.WriteLine("Pressione qualquer tecla para encerrar.");
+        }
+    }
+}
---
+++ Instituicao/Program.cs
@@ -0,0 +1,33 @@
+﻿using Instituicao.Model;
+
+Aluno aluno1 = new Aluno();
+aluno1.Cpf = 101010101;
+aluno1.Nome = "Vegas";
+aluno1.Sobrenome = "Ribeiro";
+
+Aluno aluno2 = new Aluno();
+aluno2.Cpf = 1421548;
+aluno2.Nome = "Lisa";
+aluno2.Sobrenome = "Ferreira";
+
+Aluno aluno3 = new Aluno();
+aluno3.Cpf = 14545152;
+aluno3.Nome = "Maria";
+aluno3.Sobrenome = "Morais";
+
+
+
+Matricula matriculados = new Matricula();
+matriculados.Matriculados = new List<Aluno>();
+matriculados.AlunosFormados = new List<Aluno>();
+
+matriculados.AdicionarAlunos(aluno1);
+matriculados.AdicionarAlunos(aluno2);
+matriculados.AdicionarAlunos(aluno2);
+matriculados.AdicionarFormados(aluno3);
+
+matriculados.ListarAlunos();
+matriculados.ListarFormados();
+
+
+matriculados.PercentualEvasao();
